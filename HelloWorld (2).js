1830a90567e58b99912ccb16162e028b5caf833f
Sangram Shinde <63465707+Sangram46@users.noreply.github.com>
2022-01-29 17:57:46 +0530

    Add files via upload

---
+++ HelloWorld (2).js
@@ -0,0 +1,13 @@
+var b, h, a;
+    
+    // b = base
+    // h = height
+    // a = area
+    
+    b = 42;
+    h = 12;
+    
+    // we Calculate area of the triangle
+    a = ((b * h) / 2);
+    
+    console.log("Area of the triangle = " + a + " sq. units");
