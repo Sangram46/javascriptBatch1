822aef20438c7b719944b92dc9dd0310e05b3d93
Sangram Shinde <63465707+Sangram46@users.noreply.github.com>
2022-01-29 18:10:16 +0530

    Add files via upload

---
+++ HelloWorld (4).js
@@ -0,0 +1,19 @@
+//  checks if the number is positive, negative or zero
+
+const number = 0;
+
+
+
+if (number > 0) {
+    console.log("The number is positive");
+}
+
+
+else if (number == 0) {
+  console.log("The number is zero");
+}
+
+
+else {
+     console.log("The number is negative");
+}
